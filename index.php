a57339641ba76f2ac70ab8697a24ae1f8479a552
serii <seriiburduja@gmail.com>
2020-03-16 12:53:19 +0200

    finish mobile

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <head>
     <title>Altuofianco - Webinar</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Menu</title>
     <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700" rel="stylesheet">
 
