3488e397724beb431ff699e92460014e19e1632f
serii <seriiburduja@gmail.com>
2020-03-18 13:39:30 +0200

    sub menu finish

--- js/script.js
+++ js/script.js
@@ -1,138 +1,158 @@
+var webinar1_date = ['webinar1 date1 Lunedi ', 'webinar1 date2 Giovedi'];
+var webinar2_date = ['Mercoledì 25 Marzo - ore 15.30', 'Venerdì 27 Marzo - ore 10.30'];
+var webinar3_date = ['Mercoledì 25 Marzo - ore 10.30', 'Venerdì 27 Marzo - ore 15.30'];
+var webinar4_date = ['Martedì 24 Marzo - ore 10.30', 'Giovedì 26 Marzo - ore 15.30'];
+var webinar5_date = ['webinar5 date1 Lunedi ', 'webinar5 date2 Giovedi'];
 
- 
- 		var webinar1_date= ['webinar1 date1 Lunedi ','webinar1 date2 Giovedi'];			
- 		var webinar2_date= ['Mercoledì 25 Marzo - ore 15.30','Venerdì 27 Marzo - ore 10.30'];		
- 		var webinar3_date= ['Mercoledì 25 Marzo - ore 10.30','Venerdì 27 Marzo - ore 15.30'];
-		var webinar4_date= ['Martedì 24 Marzo - ore 10.30','Giovedì 26 Marzo - ore 15.30'];
-		var webinar5_date= ['webinar5 date1 Lunedi ','webinar5 date2 Giovedi'];
-			
 function removeselect() {
-		document.getElementById("webdata").options.length = 0;
+	document.getElementById("webdata").options.length = 0;
 }
 
 
-jQuery(document).on("change",function(){
+jQuery(document).on("change", function () {
 
-		var _getValue = document.getElementById('webdata');
-		console.log(_getValue.value)
+	var _getValue = document.getElementById('webdata');
+	console.log(_getValue.value)
 
-	});
-	
-var url='';
+});
+
+var url = '';
 
 $.get(url)
-    .done(function() { 
-        // exists code 
-				
-console.log("EXIST");
+	.done(function () {
+		// exists code
 
-    }).fail(function() { 
-        // not exists code
-    })
+		console.log("EXIST");
+
+	}).fail(function () {
+	// not exists code
+})
 
 
 // Changing date and time by changing webinar
-			
- jQuery(document).on("change","#webinar",function(){
-        webinar = jQuery("#webinar").val(); 
-		console.log(webinar);
 
-let count = 0;
-	
-	if (webinar==0) {
+jQuery(document).on("change", "#webinar", function () {
+	webinar = jQuery("#webinar").val();
+	console.log(webinar);
+
+	let count = 0;
+
+	if (webinar == 0) {
 		removeselect();
 		webinar1_date.forEach(element => {
-					console.log(element)
+				console.log(element)
 				select = document.getElementById('webdata');
-		
+
 				var opt = document.createElement('option');
 				opt.value = count;
 				opt.innerHTML = element;
 				select.appendChild(opt);
 				count++;
-		}
+			}
 		);
 
 	}
-	
-	
-		
-	if (webinar==1) {
+
+
+	if (webinar == 1) {
 		removeselect();
 		webinar2_date.forEach(element => {
-					console.log(element)
+				console.log(element)
 				select = document.getElementById('webdata');
-		
+
 				var opt = document.createElement('option');
 				opt.value = count;
 				opt.innerHTML = element;
 				select.appendChild(opt);
 				count++;
-		}
+			}
 		);
 	}
-	
-		
-		
-	if (webinar==2) {
+
+
+	if (webinar == 2) {
 		removeselect();
 		webinar3_date.forEach(element => {
-					console.log(element)
+				console.log(element)
 				select = document.getElementById('webdata');
-		
+
 				var opt = document.createElement('option');
 				opt.value = count;
 				opt.innerHTML = element;
 				select.appendChild(opt);
 				count++;
-		}
+			}
 		);
 	}
-	
-	if (webinar==3) {
+
+	if (webinar == 3) {
 		removeselect();
 		webinar4_date.forEach(element => {
-					console.log(element)
+				console.log(element)
 				select = document.getElementById('webdata');
-		
+
 				var opt = document.createElement('option');
 				opt.value = count;
 				opt.innerHTML = element;
 				select.appendChild(opt);
 				count++;
-		}
+			}
 		);
 	}
-	
-		if (webinar==4) {
+
+	if (webinar == 4) {
 		removeselect();
 		webinar5_date.forEach(element => {
-					console.log(element)
+				console.log(element)
 				select = document.getElementById('webdata');
-		
+
 				var opt = document.createElement('option');
 				opt.value = count;
 				opt.innerHTML = element;
 				select.appendChild(opt);
 				count++;
-		}
+			}
 		);
 	}
 
- });
- 
- jQuery(document).ready(function($) {
- 	$('#js-button').on('click', function () {
- 		$('body').toggleClass('fixed');
- 		$(this).toggleClass('menu-opened');
- 		$('#js-navigation').toggleClass('open');
+});
+
+jQuery(document).ready(function ($) {
+	$('#js-button').on('click', function () {
+		$('body').toggleClass('fixed');
+		$(this).toggleClass('menu-opened');
+		$('#js-navigation').toggleClass('open');
 	});
- });
 
- 
- 
-(function($) {
+	let showMenu = function () {
+		if($(window).width() > 1265){
+			$('#js-navigation > .has-children ').hover(function () {
+				$('#js-navigation .sub-menu ').fadeIn(600);
+			}, function () {
+				$('#js-navigation .sub-menu ').hide();
+			});
 
- 
+			$('#js-navigation .sub-menu .has-children ').hover(function () {
+				$(this).find('.sub-menu-inner').fadeIn(300);
+			}, function () {
+				$('#js-navigation .sub-menu-inner ').hide();
+			});
+		}else{
+			$('#js-navigation > .has-children > svg ').on('click', function () {
+				$(this).next('.sub-menu').slideToggle();
+			});
+
+			$('#js-navigation .sub-menu .has-children  svg ').on('click', function () {
+				$(this).next('.sub-menu-inner').slideToggle();
+			});
+		}
+	};
+	showMenu();
+});
+
+
+(function ($) {
+
+
 })(jQuery);
 
